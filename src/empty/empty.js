a800108ec00e2e25cef9c3f43ca597275f16e48e
alistowiecki <alistowiecki>
2019-10-22 10:31:27 +0200

    Introduced typescript.

--- src/empty/empty.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Empty = () => {
-    
-    return <div className="Empty">Empty aint</div>;
-  };
-
-export default Empty;
--- src/store/actions.js
+++
@@ -1,6 +0,0 @@
-import { DISPLAY_BUILDING_DETAILS} from "./action-types";
-
-export const setDisplayBuildingDetails = building => ({
-  type: DISPLAY_BUILDING_DETAILS,
-  payload: building
-});
