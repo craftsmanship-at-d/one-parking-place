ff9ca66e775f983d97fd28167d6d6f4000af9cc6
alistowiecki <alistowiecki>
2019-10-24 08:45:14 +0200

    Tslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import {Box} from "@material-ui/core";
-import React from "react";
-import "./App.css";
-import {NavbarComponent} from "./components/nav-bar-component/NavbarComponent";
-import {ParkingComponent} from "./components/parking-component/ParkingComponent";
+import {Box} from '@material-ui/core';
+import React from 'react';
+import './App.css';
+import {NavbarComponent} from './components/nav-bar-component/NavbarComponent';
+import {ParkingComponent} from './components/parking-component/ParkingComponent';
 
 const App: React.FC = () => {
     return (
         <Box display="flex" justifyContent="flex-start">
-            <NavbarComponent></NavbarComponent>
-            <ParkingComponent></ParkingComponent>
+            <NavbarComponent/>
+            <ParkingComponent/>
         </Box>
     );
 };
