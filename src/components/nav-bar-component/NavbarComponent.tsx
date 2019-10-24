825b409097435d9dff057ec343192377677fc41e
alistowiecki <alistowiecki>
2019-10-24 08:45:14 +0200

    Tslint config

--- src/components/nav-bar-component/NavbarComponent.tsx
+++ src/components/nav-bar-component/NavbarComponent.tsx
@@ -1,23 +1,22 @@
-import {Box, Button} from "@material-ui/core";
-import React from "react";
-import {Trans, Translation} from "react-i18next";
+import { Box, Button } from '@material-ui/core';
+import React from 'react';
+import { Trans } from 'react-i18next';
 
 export class NavbarComponent extends React.Component {
 
-    public render() {
-        return <Box display="flex" flexDirection="column">
-            <Button variant="contained" href="#contained-buttons">
-                <Trans i18nKey="navbar.preview">
-                </Trans>
-            </Button>
-            <Button variant="contained" href="#contained-buttons">
-                <Trans i18nKey="navbar.reserve">
-                </Trans>
-            </Button>
-            <Button variant="contained" href="#contained-buttons">
-                <Trans i18nKey="navbar.leave">
-                </Trans>
-            </Button>
-        </Box>;
+    public render(): any {
+        return (
+            <Box display="flex" flexDirection="column">
+                <Button variant="contained" href="#contained-buttons">
+                    <Trans i18nKey="navbar.preview"/>
+                </Button>
+                <Button variant="contained" href="#contained-buttons">
+                    <Trans i18nKey="navbar.reserve"/>
+                </Button>
+                <Button variant="contained" href="#contained-buttons">
+                    <Trans i18nKey="navbar.leave"/>
+                </Button>
+            </Box>
+        );
     }
 }
