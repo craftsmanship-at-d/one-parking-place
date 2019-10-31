756cecbdb34bd65c3660e0c03e8f2313c6145ec9
tgogolewski <tomasz.gogolewski@hotmail.com>
2019-10-31 14:49:24 +0100

    Drawer instead of dialog in navbar.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { TranslationUtils } from './utils/TranslationsUtils'
 
-
 it('renders without crashing', () => {
     TranslationUtils.initializeTranslation();
     const div = document.createElement('div');
--- src/components/main-nav-bar-component/NavbarDialog.tsx
+++ src/components/main-nav-bar-component/NavbarDialog.tsx
@@ -1,45 +1,83 @@
-import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import React from 'react';
-import { NavbarContent } from '../nav-bar-component/NavbarContent';
+import { createStyles, IconButton } from '@material-ui/core'
+import Button from '@material-ui/core/Button'
+import Divider from '@material-ui/core/Divider'
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import React from 'react'
+import { Trans } from 'react-i18next'
 
 export function NavbarDialog(): JSX.Element {
 
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
+    const dialogStyles = makeStyles({
+        drawer: {
+            width: 300,
+        },
+        list: {
+            height: '90vh',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+    })
 
-    const onClick = ((event: React.MouseEvent<HTMLElement>): void => {
-        setAnchorEl(event.currentTarget);
-    });
+    const buttonStyles = makeStyles(() =>
+        createStyles({
+            button: {
+                'marginTop': '20px',
+                'width': '80%',
+                'backgroundColor': '#86BC25',
+                '&:hover': {
+                    backgroundColor: '#75787B',
+                },
+            },
+        }),
+    )
 
-    const onClose = ((): void => {
-        setAnchorEl(null);
-    });
+    const dialogCss = dialogStyles()
+    const buttonCss = buttonStyles()
+
+    const [state, setState] = React.useState({
+        open: false,
+    })
+
+    const toggle = (isOpen: boolean): () => void => {
+        return (): void => {
+            setState({ open: isOpen })
+        }
+    }
 
     return (
         <div>
             <IconButton
-                onClick={onClick}
+                onClick={toggle(true)}
             >
                 <MoreVertIcon/>
             </IconButton>
-            <Menu
-                id="long-menu"
-                keepMounted={true}
-                anchorEl={anchorEl}
-                open={open}
-                onClose={onClose}
-                // tslint:disable-next-line
-                PaperProps={{
-                    style: {
-                        height: '80%',
-                        width: 200,
-                    },
-                }}
-            >
-                <NavbarContent/>
-            </Menu>
+            <Drawer open={state.open} onClose={toggle(false)}>
+                <div
+                    className={dialogCss.drawer}
+                    role="presentation"
+                    onClick={toggle(false)}
+                >
+                    <List className={dialogCss.list}>
+                        <Button variant="contained" className={buttonCss.button}>
+                            <Trans i18nKey="navbar.preview"/>
+                        </Button>
+                        <Button variant="contained" className={buttonCss.button}>
+                            <Trans i18nKey="navbar.reserve"/>
+                        </Button>
+                        <Button variant="contained" className={buttonCss.button}>
+                            <Trans i18nKey="navbar.leave"/>
+                        </Button>
+                        <Button variant="contained" className={buttonCss.button}>
+                            <Trans i18nKey="navbar.details"/>
+                        </Button>
+                    </List>
+                    <Divider/>
+                </div>
+            </Drawer>
         </div>
-    );
+    )
 }
